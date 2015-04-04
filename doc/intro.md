576a37fcfdedc03a688f77a238e14a55839cd4ae
lenz <lenz@lenzs-MacBook-Pro.local>
2015-04-04 17:03:47 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pdfzilla
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pdfzilla/core_test.clj
@@ -0,0 +1,7 @@
+(ns pdfzilla.core-test
+  (:require [clojure.test :refer :all]
+            [pdfzilla.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
